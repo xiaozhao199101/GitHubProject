436151e9c6126036cc254c5e4c037878b4ef2c2e
zhaoshuang7 <zhaoshuang7@xiaomi.com>
2023-11-14 18:54:15 +0800

    Initial commit

---
+++ src/main/java/com/xiaozhao/BuildTools.java
@@ -0,0 +1,10 @@
+package com.xiaozhao;
+
+public class BuildTools {
+    public static void main(String[] args) {
+        System.out.println("生成工具");
+        System.out.println("一二三");
+        System.out.println("继续测试");
+        System.out.println("再来一边");
+    }
+}
---
+++ src/main/java/com/xiaozhao/Tools.java
@@ -0,0 +1,13 @@
+package com.xiaozhao;
+
+public class Tools {
+    public static void main(String[] args) {
+        System.out.println("哈哈哈");
+        System.out.println("测试版本");
+        System.out.println("这条记录是被jiayou_2023修改的");
+        System.out.println("这条记录是被jiayou_2023修改的_01");
+        System.out.println("我是master修改的");
+        System.out.println("heihei");
+        System.out.println("最后一次测试");
+    }
+}
